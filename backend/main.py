7291e49c71f53ff138385fa113548caeac9741a7
Srikant Panda <srikantap571@gmail.com>
2026-03-25 08:15:46 +0530

    Submitting latest changes

--- backend/main.py
+++ backend/main.py
@@ -46,6 +46,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5173",
         "http://[::1]:5173",
+        "https://todo-213.netlify.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
